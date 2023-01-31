61d66870d0369ae3a3554cea19c917d5d7de315e
apple <piettia320@gmail.com>
2023-01-31 07:27:33 +0100

    Login unset

--- src/main/java/com/ztm_proj/ztm_proj/ZtmProjApplication.java
+++ src/main/java/com/ztm_proj/ztm_proj/ZtmProjApplication.java
@@ -2,9 +2,9 @@ package com.ztm_proj.ztm_proj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ZtmProjApplication {
 
     public static void main(String[] args) {
